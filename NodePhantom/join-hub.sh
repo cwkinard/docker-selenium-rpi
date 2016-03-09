bf66aa28fde48c5f4a9eaac7cc956465ef539671
Will Kinard <wilsonkinard@gmail.com>
2016-03-09 04:21:41 +0100

    node waits for hub to come online

--- NodePhantom/join-hub.sh
+++ NodePhantom/join-hub.sh
@@ -1,1 +1,4 @@
-/usr/bin/phantomjs --webdriver `hostname -I | awk '{print $1}'`:8080 --webdriver-selenium-grid-hub http://hub:4444
+
+#!/bin/bash
+while ! nc -z selenium 4444; do sleep 3; done
+/usr/bin/phantomjs --webdriver `hostname -I | awk '{print $1}'`:8080 --webdriver-selenium-grid-hub http://selenium:4444
